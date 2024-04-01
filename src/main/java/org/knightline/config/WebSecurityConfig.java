eb7acaff70b7217b071d94c2bb97f9a93f4a5e59
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-01 14:09:36 -0400

    adding logic to Spring security config class WebSecurityConfig, using BCrypt password encoding

--- src/main/java/org/knightline/config/WebSecurityConfig.java
+++ src/main/java/org/knightline/config/WebSecurityConfig.java
@@ -1,5 +1,15 @@
 package org.knightline.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class WebSecurityConfig {
+
+    //Using bcrypt for password encoding
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
