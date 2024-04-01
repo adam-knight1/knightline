a5a11e660406d217f74c3fe63aa1b1bce1d3329b
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-01 19:42:28 -0400

    Added post endpoint loginUser in authController

--- src/main/java/org/knightline/controller/AuthController.java
+++ src/main/java/org/knightline/controller/AuthController.java
@@ -2,13 +2,19 @@ package org.knightline.controller;
 
 import org.knightline.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/auth")
 public class AuthController {
     @Autowired
     private UserService userService;
@@ -19,4 +25,13 @@ public class AuthController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    // User login endpoint
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestParam String email, @RequestParam String password) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, password)
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return ResponseEntity.ok("User logged in successfully");
+    }
 }
