820f0cb681d82460a5cd5973a0a5ddf79964ea4e
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-01 13:08:08 -0400

    Implementing user entity

--- src/main/java/org/knightline/repository/entity/User.java
+++ src/main/java/org/knightline/repository/entity/User.java
@@ -1,9 +1,54 @@
 package org.knightline.repository.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+//entity for storing user data in postgres
 @Entity
 @Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Type(type = "uuid-char")
+    @Column(name = "user_id", updatable = false, nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false, length = 120)
+    @NotEmpty(message = "Name is required")
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 150)
+    @NotEmpty(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @Column(nullable = false)
+    @NotEmpty(message = "Password is required")
+    private String password; // This should be encoded
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
 }
