79bedc3a16d6b7d5c1ff7fdd1107b8c6baf88e03
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-01 13:46:58 -0400

    Adding createUser logic to UserService, constructor and passwordEncoder, adding spring security gradle import

--- src/main/java/org/knightline/service/UserService.java
+++ src/main/java/org/knightline/service/UserService.java
@@ -1,4 +1,15 @@
 package org.knightline.service;
 
+import org.knightline.repository.UserRepository;
+
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService (UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+
+    }
+
 }
