694bff7ea50d438840bce70772ffb72a5de21ae9
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-01 18:05:49 -0400

    added findByEmail logic to loadUserByUsername, added constructor with user repository inject

--- src/main/java/org/knightline/securityConfig/CustomUserDetailsService.java
+++ src/main/java/org/knightline/securityConfig/CustomUserDetailsService.java
@@ -1,4 +1,7 @@
 package org.knightline.securityConfig;
+import org.knightline.repository.UserRepository;
+import org.knightline.repository.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService{
 
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found with email: " + username));
 
     }
     }
