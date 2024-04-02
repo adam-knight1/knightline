1c03e3d3ecd31903abc9ea776b3e184be16d44e6
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-02 13:30:54 -0400

    added logic for generating photo URL, added generateUniqueKey method stub to PhotoService

--- src/main/java/org/knightline/service/PhotoService.java
+++ src/main/java/org/knightline/service/PhotoService.java
@@ -1,7 +1,11 @@
 package org.knightline.service;
 
 import org.knightline.repository.PhotoRepository;
+import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
 
 public class PhotoService {
     private final S3Client s3Client;
@@ -11,4 +15,32 @@ public class PhotoService {
         this.s3Client = s3Client;
         this.photoRepository = photoRepository;
     }
+
+    public String uploadPhoto(MultipartFile file, String username) throws IOException {
+        String bucketName = "knightlinePhotos";
+        String s3Key = generateUniqueKey(file.getOriginalFilename(), username);
+        String region = "us-east-1";
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder() //creating the putObject Request
+                .bucket(bucketName)
+                .key(s3Key)
+                .build();
+        //uploading file to S3
+        s3Client.putObject(putObjectRequest, software.amazon.awssdk.core.sync.RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+        // Constructing the URL for the uploaded file
+        String photoUrl = String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, s3Key);
+
+        //save photo metadata to postgres and
+
+        return photoUrl;
+
+    }
+
+
+    private String generateUniqueKey(String originalFilename, String username){
+        return "";
+    }
+
+
 }
