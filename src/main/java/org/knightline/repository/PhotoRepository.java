b9b26b679221f7a2571b0034d79032bea84cd9f2
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-02 10:18:33 -0400

    Extended PhotoRepository with Jpa, photo and long) git status

--- src/main/java/org/knightline/repository/PhotoRepository.java
+++ src/main/java/org/knightline/repository/PhotoRepository.java
@@ -1,4 +1,9 @@
 package org.knightline.repository;
 
-public interface PhotoRepository {
+import org.knightline.repository.entity.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhotoRepository extends JpaRepository<Photo,Long> {
+
+
 }
