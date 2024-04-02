69df52c68b870487864ed2b8a7a004bd78d76ffa
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-02 12:55:48 -0400

    Added s3client, photorepository variables and constructor to PhotoService

--- src/main/java/org/knightline/service/PhotoService.java
+++ src/main/java/org/knightline/service/PhotoService.java
@@ -1,4 +1,14 @@
 package org.knightline.service;
 
+import org.knightline.repository.PhotoRepository;
+import software.amazon.awssdk.services.s3.S3Client;
+
 public class PhotoService {
+    private final S3Client s3Client;
+    private final PhotoRepository photoRepository;
+
+    public PhotoService(S3Client s3Client,PhotoRepository photoRepository){
+        this.s3Client = s3Client;
+        this.photoRepository = photoRepository;
+    }
 }
