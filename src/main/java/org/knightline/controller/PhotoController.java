ab3bac8fbcbb2a3266345993e11e35eac12218da
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-02 12:14:08 -0400

    tentative uploadPhoto endpoint created in PhotoController

--- src/main/java/org/knightline/controller/PhotoController.java
+++ src/main/java/org/knightline/controller/PhotoController.java
@@ -1,11 +1,13 @@
 package org.knightline.controller;
 
 import org.knightline.service.PhotoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.security.Principal;
+import java.util.Map;
 
 public class PhotoController {
 
@@ -16,6 +18,16 @@ public class PhotoController {
     }
 
     public ResponseEntity<?> uploadPhoto(@RequestParam("file") MultipartFile file, Principal principal) {
+        //principal variable should work to determine logged in user, contrary to local storage.
+
+        try {
+            String photoUrl = photoService.uploadPhoto(file, prinicipal.getName);
+            return ResponseEntity.ok().body(Map.of("url", photoUrl));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+
+    }
 
 
     }
