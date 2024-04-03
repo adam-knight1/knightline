36e2d8dc6ec088ce4a537ee6d62237699b1a5b74
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-03 11:24:08 -0400

    Added Family update repository query method

--- src/main/java/org/knightline/repository/FamilyUpdateRepository.java
+++ src/main/java/org/knightline/repository/FamilyUpdateRepository.java
@@ -3,8 +3,9 @@ package org.knightline.repository;
 import org.knightline.repository.entity.FamilyUpdate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FamilyUpdateRepository extends JpaRepository<FamilyUpdate,Long> {
-    List<FamilyUpdate> findAllByOrderByCreatedAtDesc
+import java.util.List;
 
+public interface FamilyUpdateRepository extends JpaRepository<FamilyUpdate,Long> {
+    List<FamilyUpdate> findAllByOrderByCreatedAtDesc();  //name derived from Spring query derivatives
 
 }
