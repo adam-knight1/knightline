b02f6f714857a7eac72fe4c26196da9a87c6301b
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-05 10:08:07 -0400

    Finished initial family update entity

--- src/main/java/org/knightline/repository/entity/FamilyUpdate.java
+++ src/main/java/org/knightline/repository/entity/FamilyUpdate.java
@@ -1,16 +1,15 @@
 package org.knightline.repository.entity;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/** this is the entity that will be used for the notification structure
+ * to be displayed in the family updates notification module after user login
+ */
 
 @Entity
 @Table(name = "family_updates")
 public class FamilyUpdate {
-
-    //body
-    //author
-    //message
-    //created at
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
@@ -21,6 +20,40 @@ public class FamilyUpdate {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
 
+    //getters + setters below
 
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
