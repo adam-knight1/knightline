4551b02c5249422c5165ea123da27a54817d65b9
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-06 18:53:20 -0400

    Implementing FamilyUpdateController.postUpdate, added update Dto

--- src/main/java/org/knightline/controller/FamilyUpdateController.java
+++ src/main/java/org/knightline/controller/FamilyUpdateController.java
@@ -1,4 +1,27 @@
 package org.knightline.controller;
 
+import org.knightline.dto.FamilyUpdateDto;
+import org.knightline.repository.entity.FamilyUpdate;
+import org.knightline.service.FamilyUpdateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**Controller class that defines various endpoints related to creating, editing, and deleting
+ * family notification updates
+ */
 public class FamilyUpdateController {
+    @Autowired
+    private final FamilyUpdateService familyUpdateService;
+
+    public FamilyUpdateController(FamilyUpdateService familyUpdateService) {
+        this.familyUpdateService = familyUpdateService;
+    }
+
+    @PostMapping
+    public ResponseEntity<FamilyUpdate> postUpdate(@RequestBody FamilyUpdateDto familyUpdateDto) {
+
+
+    }
 }
