350d967391e2388de023e9297cf880c625fc5761
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-09 18:15:34 -0400

    Beginning implementation of PhotoServiceTest

--- src/test/java/PhotoServiceTest.java
+++ src/test/java/PhotoServiceTest.java
@@ -1,2 +1,14 @@
+import org.knightline.repository.PhotoRepository;
+import org.knightline.service.PhotoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest()
 public class PhotoServiceTest {
+
+    @MockBean
+    private PhotoRepository photoRepository;
+    @Autowired
+    private PhotoService photoService;
 }
