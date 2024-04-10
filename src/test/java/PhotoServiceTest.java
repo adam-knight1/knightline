f9d88d2a23f9be18dd1412f39205361c4f8e2260
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-09 20:19:49 -0400

    Created mock variables of  userservice, s3client, photorepository in PhotoServiceTest'

--- src/test/java/PhotoServiceTest.java
+++ src/test/java/PhotoServiceTest.java
@@ -1,14 +1,30 @@
+import org.junit.jupiter.api.Test;
 import org.knightline.repository.PhotoRepository;
 import org.knightline.service.PhotoService;
+import org.knightline.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import software.amazon.awssdk.services.s3.S3Client;
 
+/** Class for testing various functions of the photo uploading mechanisms
+ *
+ */
 @SpringBootTest()
 public class PhotoServiceTest {
 
     @MockBean
     private PhotoRepository photoRepository;
+    @MockBean
+    private UserService userService;
+    @MockBean
+    private S3Client s3Client;
+
     @Autowired
     private PhotoService photoService;
+
+    @Test
+    public void uploadPhoto_ValidPhoto_UploadSuccess() {
+
+    }
 }
