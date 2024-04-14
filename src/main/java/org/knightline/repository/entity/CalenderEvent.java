fe91dafc343e0153492563c24068ad80e6e9e6c6
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-13 22:43:29 -0400

    Added calendar event entity annotations Long:Id

--- src/main/java/org/knightline/repository/entity/CalenderEvent.java
+++ src/main/java/org/knightline/repository/entity/CalenderEvent.java
@@ -1,4 +1,14 @@
 package org.knightline.repository.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="calender_events")
 public class CalenderEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+
 }
