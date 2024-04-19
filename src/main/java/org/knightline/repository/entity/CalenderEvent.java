dd9aa10ae117258d7afbaa1b1b8f1e80986102e1
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-19 09:05:58 -0400

    Adding variables to CalenderEvent entity

--- src/main/java/org/knightline/repository/entity/CalenderEvent.java
+++ src/main/java/org/knightline/repository/entity/CalenderEvent.java
@@ -1,6 +1,7 @@
 package org.knightline.repository.entity;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name="calender_events")
@@ -9,9 +10,16 @@ public class CalenderEvent {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
     @ManyToOne(fetch = FetchType.EAGER) //using eager fetch pending switch to dtos, same as FamilyUpdate
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+
 
 
 
