2fd47079505e2c69d09450106954a48f7393af5b
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-19 11:48:56 -0400

    Added CalendarController skeleton

---
+++ src/main/java/org/knightline/controller/CalendarController.java
@@ -0,0 +1,9 @@
+package org.knightline.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calendar")
+public class CalendarController {
+}
