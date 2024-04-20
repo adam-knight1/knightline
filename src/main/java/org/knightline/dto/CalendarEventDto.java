c9acdeae9dc6c456f43fea716f0f18129ef90833
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-19 20:49:18 -0400

    added CalendarEventDto

--- src/main/java/org/knightline/dto/CalendarEventDto.java
+++ src/main/java/org/knightline/dto/CalendarEventDto.java
@@ -1,4 +1,54 @@
 package org.knightline.dto;
 
+import java.time.ZonedDateTime;
+
 public class CalendarEventDto {
+    private Long id;
+    private String title;
+    private String description;
+    private ZonedDateTime eventTime;
+    private UserDto user;
+
+    public CalendarEventDto() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ZonedDateTime getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(ZonedDateTime eventTime) {
+        this.eventTime = eventTime;
+    }
+
+    public UserDto getUser() {
+        return user;
+    }
+
+    public void setUser(UserDto user) {
+        this.user = user;
+    }
 }
+
