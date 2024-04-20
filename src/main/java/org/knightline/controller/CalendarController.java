f2db47732a3c03e1e5279a1d14f6af5c93d00d96
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-19 20:55:33 -0400

    reconfiguring CalendarController.createEvent to use new calendar and user dtos

--- src/main/java/org/knightline/controller/CalendarController.java
+++ src/main/java/org/knightline/controller/CalendarController.java
@@ -1,6 +1,8 @@
 package org.knightline.controller;
 
+import org.knightline.dto.CalendarEventDto;
 import org.knightline.dto.FamilyUpdateDto;
+import org.knightline.dto.UserDto;
 import org.knightline.repository.entity.CalendarEvent;
 import org.knightline.repository.entity.FamilyUpdate;
 import org.knightline.repository.entity.User;
@@ -29,8 +31,14 @@ public class CalendarController {
     @PostMapping("/create")
     public ResponseEntity<CalendarEvent> createEvent(@RequestBody CalendarEvent calendarEvent, Principal principal) {
         User user = userService.findUserByEmail(principal.getName());
+        UserDto userDto = new UserDto(user.getUserId(),user.getName(),user.getEmail())
 
         CalendarEvent newEvent = calendarService.createCalendarEvent(user, calendarEvent.getTitle(), calendarEvent.getDescription(), calendarEvent.getEventTime());
+
+        CalendarEventDto calendarEventDto = new CalendarEventDto();
+                calendarEventDto.setEventTime(newEvent.getEventTime());
+                calendarEventDto.setDescription(newEvent.getDescription());
+                calendarEventDto.setUser(user);
 
         return ResponseEntity.ok(newEvent);
     }
