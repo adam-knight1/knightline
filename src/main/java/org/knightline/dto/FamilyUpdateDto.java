5c8c9f734120fa01567b283c9b5b4ab4d12448d2
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-04-19 21:27:22 -0400

    implemented complete FamilyUpdateDto

--- src/main/java/org/knightline/dto/FamilyUpdateDto.java
+++ src/main/java/org/knightline/dto/FamilyUpdateDto.java
@@ -3,12 +3,37 @@ package org.knightline.dto;
 import java.time.LocalDateTime;
 
 public class FamilyUpdateDto {
-    private String message;
+    private String body;
     private UserDto user;
     private LocalDateTime createdAt;
 
+    public FamilyUpdateDto(String body, UserDto user, LocalDateTime createdAt) {
+        this.body = body;
+        this.user = user;
+        this.createdAt = createdAt;
+    }
 
+    public String getBody() {
+        return body;
+    }
 
+    public void setBody(String body) {
+        this.body = body;
+    }
 
+    public UserDto getUser() {
+        return user;
+    }
 
+    public void setUser(UserDto user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
