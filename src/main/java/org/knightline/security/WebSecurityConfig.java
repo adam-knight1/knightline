4fb06b98cb84b25999e57150e3e8e755adc54c27
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-05-05 14:28:03 -0400

    Adjusted WebSecurityConfig logic to use jwtAuthenticionFilter, added JwtAuthenticationFilter class pending logic

--- src/main/java/org/knightline/security/WebSecurityConfig.java
+++ src/main/java/org/knightline/security/WebSecurityConfig.java
@@ -2,25 +2,30 @@ package org.knightline.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /** This config class is part of the spring security framework
  * It defines the parameters of the secure login process, such as what URLs/endpoints are permitted before login
  * It also contains password encoder bean, powered by BCrypt
  */
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     @Override
@@ -38,15 +43,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth/login", "/users").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/auth/**").permitAll()  // Allow all for auth paths
+                .anyRequest().authenticated()  // Other requests require authentication
                 .and()
-                .formLogin()
-                .loginPage("/login").permitAll()
-                .defaultSuccessUrl("/home", true)
-                .and()
-                .logout()
-                .permitAll();
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);  // Using stateless session; session won't be used to store user's state.
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);  // Add JWT filter pending implementation of class
     }
 
     @Bean
