d839539a7bc7ae01908b794471454aab62b8c987
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-05-05 14:10:12 -0400

    implemented index.tsx and LoginForm component, added temporary CORS configuration to auth and user controller, added JwtUtil class pending logic to handle token based user sessions

---
+++ frontend/src/components/LoginForm.tsx
@@ -0,0 +1,33 @@
+import React, { useState, FormEvent } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+
+/** Login component to handle interaction with spring security framework on the backend
+**/
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:8080/auth/login', { email, password });
+      console.log('Login successful:', response.data);
+      router.push('/home'); // Navigating to home page on successful login, instead of spring's redirect
+    } catch (error) {
+      console.error('Login error:', error.response ? error.response.data : 'Error during login');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
+      <button type="submit">Log In</button>
+    </form>
+  );
+};
+
+export default LoginForm;
--- src/main/java/org/knightline/controller/UserController.java
+++ src/main/java/org/knightline/controller/UserController.java
@@ -4,16 +4,14 @@ import org.knightline.repository.entity.User;
 import org.knightline.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Handles the endpoints for user operations, such as create user.
  * Will add more like deleting, updating etc. once MVP is finished
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") //temp pending webconfig
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
