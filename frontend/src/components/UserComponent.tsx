5c5e933cd973334c75e1348a4bb1855791538705
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-06-02 18:37:08 -0400

    Added css file for UserComponent with initial styling'

--- frontend/src/components/UserComponent.tsx
+++ frontend/src/components/UserComponent.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './UserComponent.css';
 
 type UserProps = {
   user: {
@@ -8,22 +9,25 @@ type UserProps = {
 };
 
 const UserComponent = ({ user }: UserProps) => {
-  if (!user) return null;  // Only render if user data is present
+  if (!user) return null;  // Only rendering if user data is present
 
   return (
-    <div>
-      <h1>Welcome to Knightline, {user.name}!</h1>
-      <img src={user.imageUrl} alt={`${user.name}'s profile`} style={{ width: '100px', height: '100px', borderRadius: '50%' }}/>
-      <ul>
-        <li>Family Calendar</li>
-        <li>Family Message Board</li>
-        <li>Upload Photo</li>
-        <li>Send Direct Message</li>
-        <li>Edit User Details</li>
+    <div className="text-center">
+      <h1 className="text-2xl font-bold my-4">Welcome to Knightline, {user.name}!</h1>
+      <img
+        src={user.imageUrl}
+        alt={`${user.name}'s profile`}
+        className="w-48 h-48 rounded-full mx-auto"
+      />
+      <ul className="list-none p-0 mt-4 space-y-2">
+        <li className="hover:underline">Family Calendar</li>
+        <li className="hover:underline">Family Message Board</li>
+        <li className="hover:underline">Upload Photo</li>
+        <li className="hover:underline">Send Direct Message</li>
+        <li className="hover:underline">Edit User Details</li>
       </ul>
     </div>
   );
 };
 
 export default UserComponent;
-
