e929bbd4d4a1124ce8313b7e4a51dec72429d181
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2025-01-27 22:41:22 -0500

    Testing codepipeline v2

--- frontend/src/components/PhotoUploadForm.tsx
+++ frontend/src/components/PhotoUploadForm.tsx
@@ -5,6 +5,7 @@ import Resizer from 'react-image-file-resizer';
 // Define the type for the file state
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080';
 //We'll see if this works!
+//double test
 
 const PhotoUploadForm = () => {
   const [file, setFile] = useState<File | null>(null);
