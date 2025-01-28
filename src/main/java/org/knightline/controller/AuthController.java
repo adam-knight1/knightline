b41a9a4e84738724bcefbb8180c659cb1e52cfb1
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2025-01-27 20:43:01 -0500

    Added domain to auth controller cross origin

--- src/main/java/org/knightline/controller/AuthController.java
+++ src/main/java/org/knightline/controller/AuthController.java
@@ -32,7 +32,7 @@ import java.util.Map;
  * Controller class that defines the endpoints needed for login using spring security"
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:3000") // Temporary pending webconfig
+@CrossOrigin(origins = {"http://localhost:3000", "https://knightfam.com"}) // Allow both dev and production origins
 @RequestMapping("/auth")
 public class AuthController {
     @Autowired
